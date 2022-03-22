4d5586b4c9dfeff5424aaeb90be6265a97df2647
Jola Klodowska <jola.klodowska@wp.pl>
2022-03-22 19:19:16 +0100

    Adding new column by user

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,7 +1,30 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
+import { useState } from 'react';
+import shortid from 'shortid';
 
 const List = () => {
+
+/* Stan */
+
+    const [columns, setColumns] = useState ([
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' }
+    ]);
+
+    const [value, setValue] = useState ('');
+
+/* Functions */
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setColumns([...columns, { id: shortid(), title: value }]);
+        setValue('');
+    };
+
+/* Website */
+
     return (
         <div>
             <header className={styles.header}>
@@ -9,10 +32,12 @@ const List = () => {
             </header>
             <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
-                <Column title="Books" icon="book" />
-                <Column title="Movies" icon="film" />
-                <Column title="Games" icon="gamepad" />
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
+            <form onSubmit={handleSubmit}>
+            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+                <button>Add column</button>
+            </form>
         </div>
     );
 };
