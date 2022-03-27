6e6ca69c8248b8d0dd4740f8fe6af9d419e2f2ee
Jola Klodowska <jola.klodowska@wp.pl>
2022-03-27 11:04:41 +0200

    Create new components to new subpages

---
+++ src/components/Favorite/Favorite.js
@@ -0,0 +1,12 @@
+import PageTitle from "../PageTitle/PageTitle";
+
+const Favorite = () => {
+    return (
+        <div>
+            <PageTitle>FAVORITE</PageTitle>
+            <p>Lorem Ipsum</p>
+        </div>
+    );
+}
+
+export default Favorite;
