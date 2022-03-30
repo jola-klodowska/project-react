acd2f4cad217bb67c37040817ec8984824ff6c69
Jola Klodowska <jola.klodowska@wp.pl>
2022-03-30 20:09:25 +0200

    Remove cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,24 +1,34 @@
 import clsx from 'clsx'
 import styles from './Card.module.scss'
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { toggleCardFavorite, removeCard } from '../../redux/cardsRedux';
 
 const Card = props => {
+    const cardId = props.id;
+    const cardFavorite = props.isFavorite
 
     const dispatch = useDispatch();
 
     const handleSubmit = e => {
         e.preventDefault();
-        const cardId = props.id;
-        const cardFavorite = props.isFavorite
-        dispatch(toggleCardFavorite( cardId, cardFavorite))
+        dispatch(toggleCardFavorite(cardId, cardFavorite))
+    }
+
+    const remove = e => {
+        e.preventDefault();
+        dispatch(removeCard(cardId))
     }
 
     return (
         <li className={styles.card}>{props.title}
-            <button onClick={handleSubmit} className={clsx(props.isFavorite === true && styles.active)}>
-                <i className={styles.icon + " fa fa-star-o"}></i>
-            </button>
+            <div>
+                <button onClick={handleSubmit} className={clsx(props.isFavorite === true && styles.active)}>
+                    <i className={styles.icon + " fa fa-star-o"}></i>
+                </button>
+                <button onClick={remove}>
+                    <i className={styles.icon + " fa fa-trash"}></i>
+                </button>
+            </div>
         </li>
     );
 };
