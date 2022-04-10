13025d4ea6d2458c961f3b79173bbfb082441ca4
Jola Klodowska <jola.klodowska@wp.pl>
2022-04-10 11:08:36 +0200

    Add link on Heroku

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
-import strContain from '../utils/strContains';
+
 import listsReducer from './listsRedux';
 import columnsReducer from './columnsRedux';
 import cardsReducer from './cardsRedux'
 import searchPhraseReducer from './searchPhraseRedux'
-
-//selectors
-export const getFilteredCards = ({ cards, searchPhrase }, columnId) => cards
-    .filter(card => card.columnId === columnId && strContain(card.title, searchPhrase));
-
-export const getSearchPhrase = state => state.searchPhrase;
-export const getFavoriteCards = ({ cards }) => cards.filter(card => card.isFavorite === true);
-
-// action creators
-export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
-export const addCard = payload => ({ type: 'ADD_CARD', payload })
-export const toUpdateSearchPhrase = payload => ({ type: 'UPDATE_SEARCHPHRASE', payload });
-export const toggleCardFavorite = payload => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
 
 const subreducers = {
     lists: listsReducer,
